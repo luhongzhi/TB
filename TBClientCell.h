4848dd50cab8eb77b7db1d55681b3086577e6208
ikeson <ikeson@qq.com>
2013-04-03 11:54:10 +0800

    aaaa

---
+++ TBClientCell.h
@@ -0,0 +1,13 @@
+//
+//  TBClientCell.h
+//  TBClient
+//
+//  Created by 凯旋 on 13-4-3.
+//  Copyright (c) 2013年 Beny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TBClientCell : UITableViewCell
+
+@end
---
+++ TBClientCell.m
@@ -0,0 +1,29 @@
+//
+//  TBClientCell.m
+//  TBClient
+//
+//  Created by 凯旋 on 13-4-3.
+//  Copyright (c) 2013年 Beny. All rights reserved.
+//
+
+#import "TBClientCell.h"
+
+@implementation TBClientCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
